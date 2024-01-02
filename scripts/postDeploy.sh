9d0ccd7c80e7f26b0b08a4fd45c2bc788735b42a
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-02 12:42:02 +0200

    fix

--- scripts/postDeploy.sh
+++ scripts/postDeploy.sh
@@ -4,7 +4,7 @@ nginx_conf="/opt/elestio/nginx/conf.d/${DOMAIN}.conf"
 
 sed -i '/#error_log \/var\/log\/nginx\/error_log;/a \
 \
-location ~ ^\/_matrix|\/_synapse\/client) { \
+location ~ ^\/(_matrix|_synapse\/client) { \
     proxy_pass http:\/\/172.17.0.1:8008; \
     proxy_set_header X-Forwarded-For $remote_addr; \
     proxy_set_header X-Forwarded-Proto $scheme; \
